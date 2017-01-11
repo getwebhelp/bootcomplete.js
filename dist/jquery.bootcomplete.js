a478bc35711e956ff25e195558b6acf40b13f19f
Thiago Adriano <thiago.s.adriano@gmail.com>
2017-01-11 13:35:20 -0300

    Option to bookmark and list all
    Add the markup options in the search text and start with all the records in the list

--- dist/jquery.bootcomplete.js
+++ dist/jquery.bootcomplete.js
@@ -19,29 +19,33 @@
             idFieldName : $(this).attr('name')+"_id",
             minLength : 3,
             dataParams : {},
-            formParams : {}
+            formParams: {},
+            initQuery: false,
+            queryMark: false
         }
         
         var settings = $.extend( {}, defaults, options );
-        
-        $(this).attr('autocomplete','off')
-        $(this).wrap('<div class="'+settings.wrapperClass+'"></div>')
+        var queryInit = false;
+        var that = $(this)
+        var xhr;
+
+        $(this).attr('autocomplete', 'off');
+        $(this).wrap('<div class="' + settings.wrapperClass + '"></div>');
         if (settings.idField) {
             if ($(this).parent().parent().find('input[name="' + settings.idFieldName + '"]').length !== 0) {
-                //use existing id field
             } else {
-                //there is no existing id field so create one
-                $('<input type="hidden" name="' + settings.idFieldName + '" value="">').insertBefore($(this))
+                $('<input type="hidden" name="' + settings.idFieldName + '" value="">').insertBefore($(this));
             }
         }
         $('<div class="'+settings.menuClass+' list-group"></div>').insertAfter($(this))
         
         $(this).on("keyup", searchQuery);
-        $(this).on("focusout", hideThat)
+        $(this).on("focusout", hideThat);
 
-        var xhr;
-        var that = $(this)
-
+        if (settings.initQuery) {
+            $(this).on("click", initialQueryResult);
+        }
+        
         function hideThat() {
             if ($('.list-group-item' + ':hover').length) {
                 return;
@@ -52,18 +56,18 @@
         function searchQuery(){
             
             var arr = [];
-            $.each(settings.formParams,function(k,v){
-                arr[k]=$(v).val()
-            })
+            $.each(settings.formParams, function (k, v) {
+                arr[k] = $(v).val();
+            });
             var dyFormParams = $.extend({}, arr );
-            var Data = $.extend({query: $(this).val()}, settings.dataParams, dyFormParams);
+            var Data = $.extend({ query: that.val() }, settings.dataParams, dyFormParams);
             
             if(!Data.query){
-                $(this).next('.'+settings.menuClass).html('')    
-                $(this).next('.'+settings.menuClass).hide()    
+                that.next('.' + settings.menuClass).html('');
+                that.next('.' + settings.menuClass).hide();
             }
             
-            if(Data.query.length >= settings.minLength){
+            if (queryInit || Data.query.length >= settings.minLength) {
                 
                 if(xhr && xhr.readyState != 4){
                     xhr.abort();
@@ -75,38 +79,45 @@
                     data: Data,
                     dataType: "json",
                     success: function( json ) {
-                        var results = ''
-                        $.each( json, function(i, j) {
-                            results += '<a href="#" class="list-group-item" data-id="'+j.id+'" data-label="'+j.label+'">'+j.label+'</a>'
-                        });
-                        
-                        $(that).next('.'+settings.menuClass).html(results)
-                        $(that).next('.'+settings.menuClass).children().on("click", selectResult)
-                        $(that).next('.'+settings.menuClass).show()
-                   
+                        prepareItens(json);                   
                     }
-                })
+                });
             }
+        }
+
+        function prepareItens(json) {
+            var results = '';
+            var pattern = new RegExp("(.*)(" + that.val() + ")(.*)", "gi");
+            $.each(json, function (i, j) {
+                var label = settings.queryMark ? j.label.replace(pattern, "$1<span>$2</span>$3") : j.label;
+                results += '<a href="#" class="list-group-item" data-id="' + j.id + '" data-label="' + j.label + '">' + label + '</a>'
+            });
+
+            that.next('.' + settings.menuClass).html(results);
+            that.next('.' + settings.menuClass).children().on("click", selectResult);
+            that.next('.' + settings.menuClass).show();
         }
         
         function selectResult(){
             $(that).val($(this).data('label'))
             if (settings.idField) {
                 if ($(that).parent().parent().find('input[name="' + settings.idFieldName + '"]').length !== 0) {
-                    //use existed id field
                     $(that).parent().parent().find('input[name="' + settings.idFieldName + '"]').val($(this).data('id'));
-					//ensure we trigger the onchange so we can do stuff
 					$(that).parent().parent().find('input[name="' + settings.idFieldName + '"]').trigger('change');
                 }
                 else {
-                    //use created id field
                     $(that).prev('input[name="' + settings.idFieldName + '"]').val($(this).data('id'));
-					//ensure we trigger the onchange so we can do stuff
 					$(that).prev('input[name="' + settings.idFieldName + '"]').trigger('change');
                 }
             }
             $(that).next('.' + settings.menuClass).hide();
             return false;
+        }
+
+        function initialQueryResult() {
+            queryInit = that.val() === "";
+            searchQuery();
+            queryInit = false;
         }
 
         return this;
